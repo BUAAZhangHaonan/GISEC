88deff560fc43c0c796d75ef83194bfb8933a261
BUAAZhangHaonan <zhn19375264@buaa.edu.cn>
2026-08-15 20:57:20 +0800

    refactor: remove package re-export layers

--- src/gisec/datasets/__init__.py
+++ src/gisec/datasets/__init__.py
@@ -1,19 +1,1 @@
 """Dataset and reference-bank helpers for the standalone GISEC package."""
-
-from gisec.datasets.baseline_instance_dataset import BaselineInstanceDataset
-from gisec.datasets.reference_bank import (
-    ReferenceBank,
-    ReferenceBankManifest,
-    ReferenceBankSource,
-    extract_reference_part_key,
-    load_reference_bank,
-)
-
-__all__ = [
-    "BaselineInstanceDataset",
-    "ReferenceBank",
-    "ReferenceBankManifest",
-    "ReferenceBankSource",
-    "extract_reference_part_key",
-    "load_reference_bank",
-]
--- src/gisec/engine/__init__.py
+++ src/gisec/engine/__init__.py
@@ -1,15 +1,1 @@
 """Runtime helpers for training, evaluation, and reporting."""
-
-from gisec.engine.runtime import (
-    build_benchmark_payload,
-    build_device,
-    evaluate_json,
-    write_json,
-)
-
-__all__ = [
-    "build_benchmark_payload",
-    "build_device",
-    "evaluate_json",
-    "write_json",
-]
